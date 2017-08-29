37129aae504d6c5054138712e3c2d4a7b26ff3ec
tech <techadmin@floretmedia.com>
2017-08-29 18:14:50 +0530

    no message

---
+++ 404page.php
@@ -0,0 +1,34 @@
+ <!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>Renaatus Ithaa Muiy</title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta name="robots" content="index, follow"/>
+<meta name="googlebot" content="index, follow, all"/>
+<link rel="canonical" href="http://www./" />
+<?php include("includes/csslinks.php");?>
+<body> 
+<!--================header start===========--> 
+<?php include('includes/header.php') ?>
+<!--================header end===============-->
+      
+<!--=====================section start=============-->  
+<section>
+  <div class="container">
+    <div class="wrapper mb50">      
+      <div class="ErrPg">
+        <h3><span>Sorry!!!</span> We can't find the page you're looking for.</h3>
+          <div class="Errmsg">
+            <h3>This page no longer exists.</h3>
+          </div>
+      </div>			
+    </div>
+  </div>
+</section>
+<!--==================section end===================-->
+<!--======================footer start===============-->
+<?php include("includes/footer.php");?>
+</body>
+</html>
